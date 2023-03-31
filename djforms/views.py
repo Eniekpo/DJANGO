97954a17283c5e1f6189ef3d738aed2f058a8d66
Eniekpo <johneniekpo4life@gmail.com>
2023-03-31 13:31:55 +0100

    a

--- djforms/views.py
+++ djforms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'djforms/index.html')
+
+def forms(request):
+    return render(request, 'djforms/forms.html')
